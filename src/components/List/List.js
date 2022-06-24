ed9351bcbfbb00bf11d2a98462e04fb0d7625d6c
Víctor Lázaro <vlazaro@outlook.es>
2022-06-24 17:46:58 +0200

    feat: add List component

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -3,45 +3,45 @@ import ListItem from '../ListItem/ListItem';
 import './List.css';
 
 class List extends React.Component {
-    constructor(props) {
-        super(props);
-        this.onChangeCompleted = this.onChangeCompleted.bind(this);
-        this.onRemoveItem = this.onRemoveItem.bind(this);
-    }
+  constructor(props) {
+    super(props);
+    this.onChangeCompleted = this.onChangeCompleted.bind(this);
+    this.onRemoveItem = this.onRemoveItem.bind(this);
+  }
 
-    onChangeCompleted(itemId) {
-        this.props.onChangeCompleted(itemId);
-    }
+  onChangeCompleted(itemId) {
+    this.props.onChangeCompleted(itemId);
+  }
 
-    onRemoveItem(itemId) {
-        this.props.onRemoveItem(itemId);
-    }
+  onRemoveItem(itemId) {
+    this.props.onRemoveItem(itemId);
+  }
 
-    render() {
-        const { items } = this.props;
-        const itemsLeftCount = items.filter(item => item.completed === false).length;
+  render() {
+    const { items } = this.props;
+    const itemsLeftCount = items.filter(item => item.completed === false).length;
 
-        return (
-            <div className="list">
-                <ul className="list__items">
-                    {items.length === 0 &&
-                        <li class="list__empty">The list is empty.</li>
-                    }
-                    {items.length > 0 && items.map(item =>
-                        <ListItem
-                            key={item.id}
-                            item={item}
-                            onChangeCompleted={this.onChangeCompleted}
-                            onRemoveItem={this.onRemoveItem}
-                        />
-                    )}
-                </ul>
-                <footer className="list__footer">
-                    <p className="footer__count">{itemsLeftCount} items left</p>
-                </footer>
-            </div>
-        )
-    }
+    return (
+      <div className="list">
+        <ul className="list__items">
+          {items.length === 0 &&
+            <li className="list__empty">The list is empty.</li>
+          }
+          {items.length > 0 && items.map(item =>
+            <ListItem
+              key={item.id}
+              item={item}
+              onChangeCompleted={this.onChangeCompleted}
+              onRemoveItem={this.onRemoveItem}
+            />
+          )}
+        </ul>
+        <footer className="list__footer">
+          <p className="footer__count">{itemsLeftCount} items left</p>
+        </footer>
+      </div>
+    )
+  }
 }
 
 export default List;
